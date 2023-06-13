05f0dbcc92f7e870cb692239c44ac76abba7adcb
uniqcle <uniqcle@yandex.ru>
2023-06-13 17:17:41 +0700

    open close modal

--- src/js/singleItem/singleItemController.js
+++ src/js/singleItem/singleItemController.js
@@ -2,10 +2,38 @@ import SingleItem from './singleItemModel';
 import * as view from './singleItemView';
 
 export default async function (state) {
-
+    // создаем объект singleItem
     state.singleItem = new SingleItem(state.routeParams);
 
+    // получаем данные с сервера
     await state.singleItem.getItem();
 
     view.render(state.singleItem.objectItem);
+
+    // запускаем прослушку событий
+    // 1. Открытие модального окна
+    document.querySelector('.button-order').addEventListener('click', function (e) {
+        view.showModal();
+
+    })
+
+    // 2. Закрытие модального окна. Клик по close
+    document.querySelector('.modal__close').addEventListener('click', function (e) {
+        view.hideModal();
+
+    })
+
+    // 2. Закрытие модального окна. Клик по оverlay
+    document.querySelector('.modal-wrapper').addEventListener('click', function (e) {
+        if (e.target.closest('.modal')) {
+            return null
+        } else {
+            view.hideModal();
+        }
+
+
+
+    })
+
+
 }
